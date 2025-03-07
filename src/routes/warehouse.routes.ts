509f27e99895c6e73269654046050e5aac866b1b
Yassine <medhioubyassine@gmail.com>
2025-03-07 10:26:00 +0100

    updated

--- src/routes/warehouse.routes.ts
+++ src/routes/warehouse.routes.ts
@@ -4,20 +4,20 @@ import Warehouse from '../model/warehouse';
 
 const router = Router();
 
-let isFetched = false; // Flag to track fetch status
+let isFetched = false; 
 
 router.get('/', async (req, res) => {
   try {
-    const warehouses = await Warehouse.find();
+    let warehouses = await Warehouse.find();
 
     if (warehouses.length === 0 && !isFetched) {
       console.log('⚠️ No warehouses in DB. Fetching from API...');
       await fetchAndStoreWarehouses();
-      isFetched = true; // Mark as fetched
+      isFetched = true; 
+      warehouses = await Warehouse.find(); 
     }
 
-    const storedWarehouses = await Warehouse.find(); // Get stored data
-    res.json(storedWarehouses);
+    res.json(warehouses);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching warehouses', error });
   }
