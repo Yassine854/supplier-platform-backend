a699f7c42528d763fcfe70b182d80815a2bd7336
sami <samiayachi653@gmail.com>
2025-03-10 11:29:06 +0100

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import adRoutes from './routes/ad.routes';
-
+import ScreenRoutes from './routes/Screen.routes';
 //Supplier Dashboard
 import categoryRoutes from './routes/category.routes';
 import productRoutes from './routes/product.routes';
@@ -16,9 +16,6 @@ import { authMiddleware } from './middlewares/auth.middlewares';
 
 const app = express();
 
-
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -27,17 +24,13 @@ app.use(cors());
 
 // Auth
 app.use('/api/auth', authRoutes);
-app.get('/health', (_, res) => res.json({ status: 'OK' }));
-
 
 app.get('/test', (_, res) => {
   res.json({ test: true });
 });
 app.use('/api/ad', adRoutes);
-
-
-//Supplier Dashboard
-
+app.use('/api/screen', ScreenRoutes);
+app.use('/api/products', productRoutes);
 
 const protectedRoutes = [
   { path: '/api/warehouses', route: warehouseRoutes },
@@ -49,15 +42,20 @@ const protectedRoutes = [
   { path: '/api/suppliers', route: supplierRoutes },
 ];
 
-
-
 app.use((req, res, next) => {
   const authHeader = req.headers.authorization;
-  
-  if (authHeader && !/^Bearer [A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/.test(authHeader)) {
-    return res.status(400).json({ error: 'Invalid authorization header format' });
+
+  if (
+    authHeader &&
+    !/^Bearer [A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/.test(
+      authHeader
+    )
+  ) {
+    return res
+      .status(400)
+      .json({ error: 'Invalid authorization header format' });
   }
-  
+
   next();
 });
 
